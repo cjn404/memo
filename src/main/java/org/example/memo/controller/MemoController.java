853042fa71126f8c42c2894f9c1416a6b089b457
cjn404 <moi404@naver.com>
2025-07-30 07:56:51 +0900

    feat: add get memo

--- src/main/java/org/example/memo/controller/MemoController.java
+++ src/main/java/org/example/memo/controller/MemoController.java
@@ -3,10 +3,7 @@ package org.example.memo.controller;
 import org.example.memo.dto.MemoRequestDto;
 import org.example.memo.dto.MemoResponseDto;
 import org.example.memo.entity.Memo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -32,4 +29,14 @@ public class MemoController {
 
         return new MemoResponseDto(memo);
     }
+
+    @GetMapping("/{id}")
+    public MemoResponseDto findMemoById(@PathVariable Long id) {
+
+        Memo memo = memoList.get(id);
+
+        return new MemoResponseDto(memo);
+
+    }
+
 }
